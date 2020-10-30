7e39c56484f8f5c5c9af35222e3a76f27ef76ec9
Zekeng Ndadji Milliam Maxime <ndadjimaxime@yahoo.fr>
2020-10-30 21:12:16 +0100

    Migration to a maven-based project

--- src/main/java/com/miu/smart/devtools/simpledocprocessor/DocConf.java
+++ src/main/java/com/miu/smart/devtools/simpledocprocessor/DocConf.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package simpledocprocessor;
+package com.miu.smart.devtools.simpledocprocessor;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
